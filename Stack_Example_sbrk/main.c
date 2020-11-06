44e8cac78655f6eddc77ce734aecba3c6204d022
Salvatore Bruccoleri <salvatorebrk@gmail.com>
2020-11-06 18:25:40 +0100

    Issue in delete items added comment

--- Stack_Example_sbrk/main.c
+++ Stack_Example_sbrk/main.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include "stack_sb.h"
+
+uint8_t buffer[5]={0,1,2,3,4};
 
 int main(int argc, char const *argv[])
 {
     stack_sb_p stk=stack_create();
+    stk=stack_allocate_datas(stk,sizeof(buffer),buffer);
     stk=stack_push(stk,10);
-    stk=stack_push(stk,21);
+    stk=stack_push(stk,45);
     stk=stack_push(stk,25);
     stk=stack_push(stk,45);
-    stk=stack_push(stk,24);
+    stk=stack_push(stk,44);
     stk=stack_push(stk,23);
     stk=stack_push(stk,64);
 
 
-    if(stack_element_is_in(stk,44))
+    if(stack_element_is_in(stk,4))
         printf("find\n");
     else
         printf("not found\n");
     
-   //stk=stack_delete_element(stk,44);
+ 
        printf("%d\n",stack_len(stk)); 
+       stk=stack_delete_element(stk,45); 
+
     while(stk!=NULL){
         printf("{%d}\n",(int*)stack_print_last_element(stk));
         stk=stk->next;
--- Stack_Example_sbrk/stack_sb.h
+++ Stack_Example_sbrk/stack_sb.h
@@ -1,6 +1,16 @@
+/**
+ * @file stack_sb.h
+ * @author Salvatore Bruccoleri (salvatorebrk@gmail.com)
+ * @brief Stack library example LIFO
+ * @version 0.1
+ * @date 2020-11-06
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
 #ifndef __STACK_SB_HH__
 #define __STACK_SB_HH__
-
+#include <stdint.h>
 typedef enum {
     false,
     true,
@@ -14,7 +24,6 @@ typedef struct stack_sb_t
 } stack_sb_t, *stack_sb_p;
 
 // Function Stack Prototype
-
 stack_sb_p stack_create(void);
 Boolean is_empty(stack_sb_p stk);
 Boolean stack_element_is_in(stack_sb_p st,int v);
@@ -24,5 +33,6 @@ void* stack_print_last_element(stack_sb_p st);
 stack_sb_p stack_clear(stack_sb_p st);
 stack_sb_p stack_delete_element(stack_sb_p st,int v);
 unsigned int stack_len(stack_sb_p st);
+stack_sb_p stack_allocate_datas(stack_sb_p st,int size,uint8_t *buffer);
 
 #endif
